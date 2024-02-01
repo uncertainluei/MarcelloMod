0539113b8314441d86bbf80341aed8a9a7a68e3b
LuisRandomness <73312995+LuisRandomness@users.noreply.github.com>
2024-02-01 15:45:38 +0000

    remove forge version artifacts

--- src/main/java/me/luisrandomness/marcellomod/core/blocks/MarcelloTypeBlock.java
+++
@@ -1,9 +0,0 @@
-package me.luisrandomness.marcellomod.core.blocks;
-
-import net.minecraft.block.Block;
-
-public class MarcelloTypeBlock extends Block {
-    public MarcelloTypeBlock(Properties properties) {
-        super(properties);
-    }
-}
--- src/main/java/me/luisrandomness/marcellomod/core/types/MM_CreatureTypes.java
+++
@@ -1,7 +0,0 @@
-package me.luisrandomness.marcellomod.core.types;
-
-import net.minecraft.entity.CreatureAttribute;
-
-public class MM_CreatureTypes {
-    public static final CreatureAttribute MARCELLO = new CreatureAttribute();
-}
